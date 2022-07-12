8110d4b9a666f967ca0d30164dd5bffcde75f73b
Muzammil803 <syedmuzammil691@gmail.com>
2022-07-12 23:37:37 +0500

    complete

---
+++ app.js
@@ -0,0 +1,67 @@
+// var date = new Date();
+// document.write(date);
+// var arr = ["January","February","March","April","May","June","July","August","September	","October","November","December"];
+// var date = new Date();
+// var month = date.getMonth();
+
+// console.log(month);
+// document.write("<h1> Current Month : " + arr[month] + "</h1>");
+// var arr = ["sun","mon","tues","wed","thur","fri","sat"];
+// var date = new Date();
+// var day = date.getDay();
+// alert( "Today is " + arr[day]);
+// var funDay = arr[day];
+// if(funDay === "sat" || funDay === "sun"){
+//     document.write("<h1> Its Fun Day </h1>");
+// }
+// var date = new Date();
+// var month = date.getMonth();
+// if(month < 15){
+//     document.write("<h1> First Fifteen Days Of Month")
+
+
+// }else{
+//     document.write("<h1> Last Days Of Month");
+// }
+// var date = new Date();
+// var milliSecond = date.getTime();
+// var minutes = milliSecond /60000;
+// document.write("<h1> Current Date :" + date + "</h1>");
+// document.write("<h1>Elapsed milliseconds since 1 january 1970:" + milliSecond + "</h1>");
+// document.write("<h1>Elapsed Minutes since 1 january 1970: " + minutes.toFixed(0) + "</h1>");
+// var date = new Date();
+// var time = date.getHours();
+// if(time < 13){
+//     document.write("<h1> Its AM </h1>");
+// }else{
+//     document.write("<h1> Its PM </h1>");
+
+// }
+// var laterDate = new Date("31 december 2021");
+// document.write("<h1> Later date:" + laterDate + "</h1>"); 
+// var date = new Date("1 january 2015").getTime();
+// var ramdanDate = new Date("18 june 2015").getTime();
+// var milliSecond = ramdanDate - date;
+// var day = milliSecond / (1000 * 60 * 60 * 24);
+// document.write("<h1>" + day + " days passed since 1st Ramadan </h1>");
+// var date = new Date("1 january 2015").getSeconds();
+// var refDate = new Date("5 december 2015").getSeconds();
+// var sec = refDate - date;
+// document.write("<h1> On reference date " + sec + " passed on the begining of 2015");
+// var date =new Date();
+// var lastDate = new Date("8 july 1922");
+// document.write("<h1> Current Date: " + date + "</h1>");
+// document.write("<h1> Current Date: " + lastDate + "</h1>");
+// var date = new Date().getTime();
+// var age = prompt("Enter age year");
+// var ageDate = new Date(age).getTime();
+// var userAge = (date - ageDate) / (1000 * 60 * 60 * 24 * 365);
+// document.write("<h1> your Age is " + Math.round(userAge) + "</h1>");
+// document.write("<h1> Your Birth year is: " + age + "</h1>");
+// document.write("<h1> Customer Name: ABC</h1>")
+// document.write("<h1> Month: February</h1>")
+// document.write("<h1> Number of Units: 410</h1>")
+// document.write("<h1> Charges per unit: 16</h1>")
+// document.write("<h1> Net Amount Payable(within due date): 6550</h1>")
+// document.write("<h1> late payment surcharge: 359</h1>")
+// document.write("<h1> Gross Amount Payable (after due date): 6910</h1>")
